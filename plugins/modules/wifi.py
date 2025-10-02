d499b5b20d5efd2677200faad6b2d85a06984daf
Erki <skull132@users.noreply.github.com>
2025-10-02 21:59:15 +0300

    Update wifi.py

--- plugins/modules/wifi.py
+++ plugins/modules/wifi.py
@@ -1,5 +1,144 @@
 #!/usr/bin/python
 
+DOCUMENTATION = '''
+---
+module: wifi
+short_description: A module for controlling the WiFi settings on a Shelly gen 2+ device.
+version_added: "0.0.1"
+description:
+  - A module for controlling the WiFi settings on a Shelly gen 2+ device.
+  -
+  - The configuration of a Shelly's WiFi interface has 4 sections: "ap" for the access point it
+  - can create itself. "sta" and "sta1" for WiFi client connections. And "roam" for the roaming settings.
+  - This plugin lets you configure these individually.
+options:
+    configuring:
+        description:
+          - Determines which subsystem is being configured.
+        required: true
+        type: str
+        choices: ["ap", "sta", "sta1", "roam"]
+    timeout:
+        description:
+          - In milliseconds. Determines how long to wait until the module should become available again.
+          - If the device is not available after the time specified, the module fails.
+        required: false
+        type: int
+        default: 5000
+    ssid:
+        description:
+          - The SSID for a given connection.
+          - Required if configuring is "ap", "sta", or "sta1".
+          -
+          - If configuring is "ap", then this controls the SSID of the hosted WiFi network.
+          - If configuring is "sta" or "sta1", then this controls the SSID of the WiFi network with which the
+          - interface should connect to.
+        required: false
+        type: str
+    password:
+        description:
+          - The password for a given connection.
+          - Required if configuring is "ap", "sta", or "sta1".
+          -
+          - If configuring is "ap", then this controls the password of the hosted WiFi network.
+          - If configuring is "sta" or "sta1", then this controls the password of the WiFi network with which the
+          - interface should connect to.
+        required: false
+        type: str
+    is_open:
+        description:
+          - True if the access point is open, false otherwise.
+          - Required if configuring is "ap".
+        required: false
+        type: bool
+    enable:
+        description:
+          - Controls whether or not the interface is enabled.
+          - Required if configuring is "ap", "sta",  or "sta1".
+        required: false
+        type: bool
+    range_extender:
+        description:
+          - Controls whether the range extender is enabled or not.
+          - Not all Shelly devices have a range extender.
+        required: false
+        type: bool
+    ipv4mode:
+        description:
+          - Sets the IPv4 mode of the device. Must be either "dhcp" or "static".
+          - Required if configuring is "sta" or "sta1".
+        required: false
+        type: str
+        choices: ["dhcp", "static"]
+    ip:
+        description:
+          - Sets the IPv4 address that's used if ipv4mode is "static".
+          - Required if ipv4mode is "static".
+        required: false
+        type: str
+    netmask:
+        description:
+          - Sets the netmask that's used if ipv4mode is "static".
+          - Required if ipv4mode is "static".
+        required: false
+        type: str
+    gw:
+        description:
+          - Sets the gateway that's used if ipv4mode is "static".
+          - Required if ipv4mode is "static".
+        required: false
+        type: str
+    nameserver:
+        description:
+          - Sets the nameserver address that's used if ipv4mode is "static".
+          - Required if ipv4mode is "static".
+        required: false
+        type: str
+    rssi_thr:
+        description:
+          - Sets the RSSI threshold at which roaming is triggered.
+          - Required if configuring is "roam".
+        required: false
+        type: int
+    interval:
+        description:
+          - Sets the interval with which to scan for better access points.
+          - Set to 0 to disable.
+          - Required if configuring is "roam".
+        require: false
+        type: int
+author:
+    - RustedSkull (@skull132)
+'''
+
+EXAMPLES = '''
+# Disable the access point.
+- name: Disable AP
+  enclave.shelly.wifi:
+    configuring: ap
+    ssid: ""
+    password: ""
+    is_open: false
+    enable: false
+
+# Set the primary WiFi connection to some network.
+- name: Primary WiFi interface connection
+  enaclave.shelly.wifi:
+    configuring: sta
+    ssid: my_home
+    password: some_secret
+    enable: true
+    ipv4mode: dhcp
+'''
+
+RETURN = '''
+restarted:
+  description: Set to true if the module issued the restart command to the Shelly device.
+  type: bool
+  returned: always
+'''
+
+
 from dataclasses import dataclass, asdict
 from typing import Optional
 
@@ -66,6 +205,7 @@ def compose_module_args():
     module_args.update(ap_args)
 
     sta_args = shelly_utils.dataclass_into_ansible_spec(StaConfig)
+    sta_args["ipv4mode"]["choices"] = ["dhcp", "static"]
     module_args.update(sta_args)
 
     roaming_args = shelly_utils.dataclass_into_ansible_spec(RoamConfig)
@@ -81,9 +221,10 @@ def run_module():
         supports_check_mode=True,
         required_if=[
             ("configuring", "ap", ("ssid", "password", "is_open", "enable")),
-            ("configuring", "sta", ("ssid", "password", "is_open", "enable", "ipv4mode")),
-            ("configuring", "sta1", ("ssid", "password", "is_open", "enable", "ipv4mode")),
-            ("configuring", "roam", ("rssi_thr", "interval"))
+            ("configuring", "sta", ("ssid", "password", "enable", "ipv4mode")),
+            ("configuring", "sta1", ("ssid", "password", "enable", "ipv4mode")),
+            ("configuring", "roam", ("rssi_thr", "interval")),
+            ("ipv4mode", "static", ("ip", "netmask", "gw", "nameserver"))
         ]
     )
 
